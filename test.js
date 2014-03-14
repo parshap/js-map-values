780d54af5af2699d90bebcb04be0cbcd65ca399b
Parsha Pourkhomami <parshap+git@gmail.com>
2014-03-14 00:51:58 -0700

    Remove console.log call

--- test.js
+++ test.js
@@ -8,7 +8,6 @@ test("should map object", function(t) {
 	var obj = map({ foo: 2, bar: 5 }, function(val) {
 		return val * 2;
 	});
-	console.log(obj);
 	assert(Object.keys(obj).length === 2);
 	assert(obj.foo === 4);
 	assert(obj.bar === 10);
